de1a178bd374c0eeec2d064b45b16854b4e9cbe4
Faris <octaflop@gmail.com>
2010-04-04 23:26:55 -0700

    outlining redis plans

--- models.py
+++ models.py
@@ -1,10 +1,28 @@
 import random
+"""
+Here's kinda what I'd like to do:
+
+r.zadd("site:%s" % id, (r.lrange("post:%s:comment" % id, 0, 0)), 1)
+# or
+r.zadd("site:%s" % site_id, (r.lrange("post:%s:comment" % id, 0, # key 
+(r.get("global:settings:maxfrontcomments")), # max range
+(r.get("post:%s:rank" % id))) # incr rank in cardinal list by a pulled amount
+
+ID: <year>.<month>.<day>.<incr>.<time>
+ex: 2010.04.24.1.232505
+
+Site will need a unique hash. Script will initiate through a "site" list with a
+hashed id.
+"""
 class Post(object):
     def __init__(self, title, content, imageref, author)
         self.author = author
         self.title = title
         self.content = content
         self.datepub = datetime.datetime.now()
+
+    def __repr__():
+        # return something
 
     def new():
         # make a new post
